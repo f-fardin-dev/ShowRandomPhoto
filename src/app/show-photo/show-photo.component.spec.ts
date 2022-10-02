17b16ad2b88c857a3fb869528c535e2b3971b0cd
Fardin <fakurian.fardin@gmail.com>
2022-10-02 13:59:27 +0330

    Add show-photo component

---
+++ src/app/show-photo/show-photo.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowPhotoComponent } from './show-photo.component';
+
+describe('ShowPhotoComponent', () => {
+  let component: ShowPhotoComponent;
+  let fixture: ComponentFixture<ShowPhotoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShowPhotoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShowPhotoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
