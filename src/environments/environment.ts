a69b11c40cada0da8f2ef6456c589c24e1b2255c
Fardin <fakurian.fardin@gmail.com>
2022-10-02 14:33:10 +0330

    Add api key of unsplash

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  unsplashAccessKey: 'your access key',
+  unsplashSecretKey: 'your secret key',
 };
 
 /*
