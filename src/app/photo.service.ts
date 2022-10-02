8dcc8046b3285c30b82ad013cc8718e47cf03660
Fardin <fakurian.fardin@gmail.com>
2022-10-02 14:51:29 +0330

    Update photo service
    pipe the response

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -1,7 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -9,10 +15,12 @@ export class PhotoService {
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get('https://api.unsplash.com/photos/random', {
-      headers: {
-        Authorization: `Client-ID ${environment.unsplashAccessKey}`,
-      },
-    });
+    return this.http
+      .get<UnsplashResponse>('https://api.unsplash.com/photos/random', {
+        headers: {
+          Authorization: `Client-ID ${environment.unsplashAccessKey}`,
+        },
+      })
+      .pipe(map((res) => res?.urls?.regular));
   }
 }
