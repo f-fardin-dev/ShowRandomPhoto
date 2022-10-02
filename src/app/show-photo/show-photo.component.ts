1a6e86dcceb59ad360cfaf2c777f1b82ccfd8e2f
Fardin <fakurian.fardin@gmail.com>
2022-10-02 15:00:13 +0330

    Update show-photo component
    Add photo service and some html to show the photo

--- src/app/show-photo/show-photo.component.ts
+++ src/app/show-photo/show-photo.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo.service';
 
 @Component({
   selector: 'app-show-photo',
   templateUrl: './show-photo.component.html',
-  styleUrls: ['./show-photo.component.css']
+  styleUrls: ['./show-photo.component.css'],
 })
 export class ShowPhotoComponent implements OnInit {
+  imgUrl: string = '';
 
-  constructor() { }
+  constructor(private photoService: PhotoService) {}
 
   ngOnInit(): void {
+    this.getNewPhoto();
   }
 
+  getNewPhoto() {
+    this.photoService.getPhoto().subscribe((url) => (this.imgUrl = url));
+  }
 }
