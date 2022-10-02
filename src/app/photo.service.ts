0ca6b554f27a56f32fd349cb9b6a9bc75a8e0acb
Fardin <fakurian.fardin@gmail.com>
2022-10-02 14:41:23 +0330

    Update photo service
    Add getPhoto method

--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: `Client-ID ${environment.unsplashAccessKey}`,
+      },
+    });
+  }
 }
